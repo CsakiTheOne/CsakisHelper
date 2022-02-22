0bbe7a6ae1169c2c12cf766e7383240b2ccb69c4
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2022-02-22 08:52:51 +0100

    FCM API added, I don't know if it works

--- CsakisHelperLibrary/src/main/java/com/csakitheone/csakishelperlibrary/FCMAPI.kt
+++ CsakisHelperLibrary/src/main/java/com/csakitheone/csakishelperlibrary/FCMAPI.kt
@@ -5,21 +5,23 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
-class FCMLegacyAPI(private val serverKey: String) {
+class FCMAPI(private val firebaseProjectId: String, oauthToken: String) {
+
+    private val token = if (oauthToken.startsWith("Bearer")) oauthToken
+    else "Bearer $oauthToken"
 
     fun sendData(context: Context, targetTopic: String, data: Map<String, Any>) {
-        val topic = "/topics/$targetTopic"
-        val api = "https://fcm.googleapis.com/fcm/send"
+        val api = "https://fcm.googleapis.com/v1/projects/$firebaseProjectId/messages:send"
 
-        val outerData = JSONObject(mapOf(
-            "to" to topic,
+        val outerData = JSONObject(mapOf("message" to mapOf(
+            "topic" to targetTopic,
             "data" to data,
-        ))
+        )))
 
         val jsonObjectRequest = object: JsonObjectRequest(Method.POST, api, outerData, { }, { }) {
             override fun getHeaders(): MutableMap<String, String> {
                 return mutableMapOf(
-                    "Authorization" to "key=$serverKey",
+                    "Authorization" to "key=$token",
                     "Content-Type" to "application/json"
                 )
             }
@@ -28,22 +30,23 @@ class FCMLegacyAPI(private val serverKey: String) {
         Volley.newRequestQueue(context).add(jsonObjectRequest)
     }
 
-    fun sendNotification(context: Context, targetTopic: String, title: String, body: String? = null) {
-        val topic = "/topics/$targetTopic"
-        val api = "https://fcm.googleapis.com/fcm/send"
+    fun sendNotification(context: Context, targetTopic: String, data: Map<String, Any>, title: String, body: String? = null) {
+        val api = "https://fcm.googleapis.com/v1/projects/$firebaseProjectId/messages:send"
 
         val notificationData = mutableMapOf(
             "title" to title,
         )
         if (!body.isNullOrEmpty()) notificationData["body"] = body
 
-        val outerData = JSONObject("""{"to": "$topic"}""")
-        outerData.put("notification", notificationData)
+        val outerData = JSONObject(mapOf("message" to mapOf(
+            "topic" to targetTopic,
+            "notification" to notificationData,
+        )))
 
         val jsonObjectRequest = object: JsonObjectRequest(Method.POST, api, outerData, { }, { }) {
             override fun getHeaders(): MutableMap<String, String> {
                 return mutableMapOf(
-                    "Authorization" to "key=$serverKey",
+                    "Authorization" to "key=$token",
                     "Content-Type" to "application/json"
                 )
             }
